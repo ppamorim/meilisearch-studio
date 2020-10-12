5729495bdce3f995adb6dba5d1609a2357d40ac9
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-10-12 03:26:48 +0100

    Adding icon

--- MeiliSearch Studio/MenuViewController.swift
+++ MeiliSearch Studio/MenuViewController.swift
@@ -9,6 +9,7 @@ import Cocoa
 
 struct Page {
   let title: String
+  let icon: String
   let viewControllerName: String
 }
 
@@ -17,10 +18,10 @@ final class MenuViewController: NSViewController {
   private weak var homeViewControllerDelegate: HomeViewControllerDelegate?
 
   fileprivate let items: [Page] = [
-    Page(title: "Indexes", viewControllerName: String(describing: IndexesViewController.self)),
-    Page(title: "Documents", viewControllerName: String(describing: DocumentsViewController.self)),
-    Page(title: "Search", viewControllerName: String(describing: IndexesViewController.self)),
-    Page(title: "Settings", viewControllerName: String(describing: IndexesViewController.self))
+    Page(title: "Indexes", icon: "staroflife", viewControllerName: String(describing: IndexesViewController.self)),
+    Page(title: "Documents", icon: "doc.text.magnifyingglass", viewControllerName: String(describing: DocumentsViewController.self)),
+    Page(title: "Search", icon: "", viewControllerName: String(describing: IndexesViewController.self)),
+    Page(title: "Settings", icon: "", viewControllerName: String(describing: IndexesViewController.self))
   ]
 
   @IBOutlet weak var tableView: NSTableView!
@@ -43,9 +44,24 @@ final class MenuViewController: NSViewController {
 
 extension MenuViewController: NSTableViewDelegate {
 
+  func tintedImage(_ image: NSImage?, tint: NSColor) -> NSImage? {
+      guard let uimage = image, let tinted = uimage.copy() as? NSImage else { return image }
+      tinted.lockFocus()
+      tint.set()
+
+      let imageRect = NSRect(origin: NSZeroPoint, size: uimage.size)
+      imageRect.fill(using: .sourceAtop)
+
+      tinted.unlockFocus()
+      return tinted
+  }
+
   func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
     if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Item"), owner: nil) as? NSTableCellView {
-      cell.textField?.stringValue = items[row].title
+      let item = items[row]
+      let image: NSImage? = !item.icon.isEmpty ? NSImage(named: item.icon) : nil
+      cell.imageView?.image = tintedImage(image, tint: NSColor.white)
+      cell.textField?.stringValue = item.title
       return cell
     }
     return nil
